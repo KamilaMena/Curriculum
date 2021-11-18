d945f044816cb3a994efe5aa0c1dcf126bd37fed
KamilaMena <kamilaa.mena@gmail.com>
2021-11-18 15:33:53 -0500

    Base de slider listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,31 @@ $(document).ready(main);
         }
     });
 });*/
+
+let slider = document.querySelector(".slider__contenedor")
+let sliderind = document.querySelectorAll(".slider__content")
+let contador = 1;
+let width = sliderind[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize", function(){
+    let width = sliderind[0].clientWidth;
+})
+
+setInterval(function(){
+    slides()
+}, intervalo);
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .7s";
+    contador++;
+
+    if(contador == sliderind.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        }, 1500)
+    }
+}
