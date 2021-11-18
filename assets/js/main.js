d323a5b7c6996cfdbad8cb4b397a900ea6dbbd0a
KamilaMena <kamilaa.mena@gmail.com>
2021-11-18 14:37:08 -0500

    Pruebas en js

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*// NAVBAR
+
+
+$(document).ready(main);
+
+    var contador = 1;
+
+    function main() {
+        $('.menu').click(function(){
+            $('navbar__items').toggle();
+        });
+    }
+
+/*('document').ready(function($) {
+    var menubtn = $('.menu');
+    menu1 = $('.navbar__menu');
+
+    menubtn.click(function() {
+        if(menu1.hasClass('show')) {
+
+            menu1.removeClass('show');
+        } else {
+            menu1.addClass('show');
+        }
+    });
+});*/
