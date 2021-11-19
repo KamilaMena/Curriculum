45ed017d8c254d54c9a58252bba5b36362ddd1db
KamilaMena <kamilaa.mena@gmail.com>
2021-11-19 09:09:24 -0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,6 @@
-/*// NAVBAR
 
 
-$(document).ready(main);
-
-    var contador = 1;
-
-    function main() {
-        $('.menu').click(function(){
-            $('navbar__items').toggle();
-        });
-    }
-
-/*('document').ready(function($) {
-    var menubtn = $('.menu');
-    menu1 = $('.navbar__menu');
-
-    menubtn.click(function() {
-        if(menu1.hasClass('show')) {
-
-            menu1.removeClass('show');
-        } else {
-            menu1.addClass('show');
-        }
-    });
-});*/
+/* SLIDER */
 
 let slider = document.querySelector(".slider__contenedor")
 let sliderind = document.querySelectorAll(".slider__content")
